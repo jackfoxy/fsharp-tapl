63cf6a2cc674025ca5973a9146315397a909dc5c
Jack Fox <jackfoxy@gmail.com>
2016-04-03 12:54:04 -0700

    migrate to project scaffold format

---
+++ tests/fsharp-tapl.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module fsharp-tapl.Tests
+
+open fsharp-tapl
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
