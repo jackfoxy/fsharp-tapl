7c273d98ae02b5bbaa85d4455c84abb8e866bcf7
Jack Fox <jackfoxy@gmail.com>
2018-02-24 12:24:12 -0800

    paket magic mode

--- build.cmd
+++ build.cmd
@@ -1,18 +1,13 @@
 @echo off
 cls
 
-.paket\paket.bootstrapper.exe
-if errorlevel 1 (
-  exit /b %errorlevel%
-)
-
-.paket\paket.exe restore
+.\paket.exe restore
 if errorlevel 1 (
   exit /b %errorlevel%
 )
 
 IF NOT EXIST build.fsx (
-  .paket\paket.exe update
+  .\paket.exe update
   packages\build\FAKE\tools\FAKE.exe init.fsx
 )
 packages\build\FAKE\tools\FAKE.exe build.fsx %*
